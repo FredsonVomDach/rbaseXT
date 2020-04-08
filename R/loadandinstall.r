6c040896cc446d06ebd12ad057261ac926be1c02
Frederic Schwarzenbacher <f_schwarzenbacher@live.de>
2020-04-08 15:10:47 +0200

    corrected function export for package building

--- R/loadandinstall.r
+++ R/loadandinstall.r
@@ -1,3 +1,16 @@
+#' @title Load and install packages
+#' @description Combines the library() and install.packages() comands in one function
+#'
+#' @param mypkg String. Single Character String of package name or vector of package names
+#'
+#' @author Frederic Schwarzenbacher
+#'
+#' @examples
+#' loadandinstall("packagename")
+#' @keywords load and install library install.packages#'
+#' @export
+
+
 loadandinstall <- function(mypkg) {
   for (i in c(1:length(mypkg))){
     if (!is.element(mypkg[i], installed.packages()[,1])){
