666d0939952a77a882f17d5f332318c820ca59ee
Frederic Schwarzenbacher <f_schwarzenbacher@live.de>
2020-05-01 15:23:12 +0200

    Implemented function to reinstall packages when loaded failed

--- R/loadandinstall.r
+++ R/loadandinstall.r
@@ -12,10 +12,53 @@
 
 
 loadandinstall <- function(mypkg) {
+  
+  errorhandler <- function(pck,task) {
+    success <- tryCatch(
+      {
+        if (task=="install") {
+          install.packages(pck)
+        }
+        if (task=="load") {
+          library(pck, character.only=TRUE)
+        }
+        status=1
+      },
+      error=function(cond) {
+        status=0
+      },
+      warning=function(cond){
+        status=0
+      }
+    )    
+    return(success)
+  }
+  
+  errors <- c()
   for (i in c(1:length(mypkg))){
     if (!is.element(mypkg[i], installed.packages()[,1])){
-      install.packages(mypkg[i])}
+      task="install"
+      success <- errorhandler(mypkg[i],task)
+      if (success==0) {
+        errors <- append(errors,mypkg[i])
+        #next() installation errors are not always catched, try to load as a next step
+      }
+    }
     
-    library(mypkg[i], character.only=TRUE)
+    task="load"
+    success <- errorhandler(mypkg[i],task)
+    if (success==0) {
+      task="install"
+      errorhandler(mypkg[i],task)
+      task="load"
+      success <- errorhandler(mypkg[i],task)
+      if (success==0) {
+        errors <- append(errors,mypkg[i])
+      }
+    }
+  }
+  if (!is.null(errors)) {
+    message("The were errors or warnings while installing or loading the follwing package(s)")
+    return(unique(errors))
   }
 }
